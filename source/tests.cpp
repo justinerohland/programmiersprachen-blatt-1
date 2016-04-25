e84de3a063a706a1620349f9127e7809fc9677b4
justinerohland <justine.rohland@uni-weimar.de>
2016-04-25 16:01:07 +0200

    tests.cpp

--- source/tests.cpp
+++ source/tests.cpp
@@ -4,14 +4,22 @@
 
 int gcd(int a, int b)
 {
-	return 1;
+
+	int ggt = 1;
+
+	for (int i = 2; i <= a && i <= b; i++){
+		if (a%i == 0 && b%i == 0){
+			ggt = i;
+		}
+	}
+	return ggt;
 }
 
 TEST_CASE("describe_gcd", "[gcd]")
 {
-	REQUIRE(gcd(2,4) == 2)
-	REQUIRE(gcd(6,9) == 3)
-	REQUIRE(gcd(3,7) == 1)
+	REQUIRE(gcd(2,4) == 2);
+	REQUIRE(gcd(6,9) == 3);
+	REQUIRE(gcd(3,7) == 1);
 }
 
 int main(int argc, char* argv[])
